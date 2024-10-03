290b10b13a2c59904e30a3dc3c93916cf64a45dc
Paul <paulcarva2@hotmail.com>
2024-10-03 13:42:52 +0200

    Parte_acabada_falta_Windows

--- SOL/practica2_3/src/vistacontrolador/Combobox.java
+++ SOL/practica2_3/src/vistacontrolador/Combobox.java
@@ -13,19 +13,30 @@ import javax.swing.JOptionPane;
  */
 public class Combobox extends javax.swing.JFrame {
 
-    private static ArrayList<String> modulos_matriculados = new ArrayList();
+    private static ArrayList<String> modulos_DAM1 = new ArrayList();
+    private static ArrayList<String> modulos_DAM2 = new ArrayList();
 
     public Combobox() {
         initComponents();
         //modulos harcodeados para agregarlos directamente
 
-        modulos_matriculados.add("Programacion de Servicios y Procesos");
-        modulos_matriculados.add("Sistemas de Gestion Empresarial");
-        modulos_matriculados.add("Desarrollo de Interfaces");
-        modulos_matriculados.add("Programacion Multimedia");
-        modulos_matriculados.add("Acceso a Datos");
-        modulos_matriculados.add("Ingles");
-        modulos_matriculados.add("Empresas");
+        modulos_DAM2.add("Programacion de Servicios y Procesos");
+        modulos_DAM2.add("Sistemas de Gestion Empresarial");
+        modulos_DAM2.add("Desarrollo de Interfaces");
+        modulos_DAM2.add("Programacion Multimedia");
+        modulos_DAM2.add("Acceso a Datos");
+        modulos_DAM2.add("Ingles");
+        modulos_DAM2.add("Empresas");
+
+        Curso_Combobox.addItem("DAM1");
+        Curso_Combobox.addItem("DAM2");
+
+        modulos_DAM1.add("Programacion");
+        modulos_DAM1.add("Sistemas Informaticos");
+        modulos_DAM1.add("FOL");
+        modulos_DAM1.add("Lenguaje de Marcas");
+        modulos_DAM1.add("Bases de Datos");
+        modulos_DAM1.add("Entornos de Desarrollo");
     }
 
     /**
@@ -43,10 +54,13 @@ public class Combobox extends javax.swing.JFrame {
         Modulos_Combobox = new javax.swing.JComboBox<>();
         Agregar_Modulos = new javax.swing.JButton();
         Borrar_Modulos = new javax.swing.JButton();
+        Curso_Combobox = new javax.swing.JComboBox<>();
+        Borrar_uno_boton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Combobox_Modulos");
 
-        Texto_Modulos.setText("Introducir los modulos que estas matriculado:");
+        Texto_Modulos.setText("Introduzca el nombre de los módulos y el curso que estas matriculado:");
 
         Añadir_Boton.setText("Añadir");
         Añadir_Boton.addActionListener(new java.awt.event.ActionListener() {
@@ -81,39 +95,64 @@ public class Combobox extends javax.swing.JFrame {
             }
         });
 
+        Curso_Combobox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Curso_ComboboxActionPerformed(evt);
+            }
+        });
+
+        Borrar_uno_boton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Paul\\Documents\\GitHub\\pr-ctica-2-3-elementos-combobox-PaulCarvajal\\SOL\\practica2_3\\src\\vistacontrolador\\trash.png")); // NOI18N
+        Borrar_uno_boton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                Borrar_uno_botonActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(24, 24, 24)
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(Texto_Modulos)
+                    .addComponent(Texto_Modulos, javax.swing.GroupLayout.PREFERRED_SIZE, 382, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(Campo_modulos, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(Añadir_Boton))
+                            .addComponent(Añadir_Boton)
+                            .addComponent(Campo_modulos, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(Curso_Combobox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(Modulos_Combobox, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(Borrar_Modulos, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(Agregar_Modulos, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
-                .addContainerGap(20, Short.MAX_VALUE))
+                            .addComponent(Agregar_Modulos)
+                            .addComponent(Borrar_Modulos, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(Borrar_uno_boton, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(12, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(123, Short.MAX_VALUE)
+                .addContainerGap(73, Short.MAX_VALUE)
                 .addComponent(Texto_Modulos)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Campo_modulos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Modulos_Combobox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(31, 31, 31)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Añadir_Boton)
-                    .addComponent(Agregar_Modulos))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(33, 33, 33)
+                        .addComponent(Curso_Combobox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(Campo_modulos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(31, 31, 31)
+                        .addComponent(Añadir_Boton))
+                    .addGroup(layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(Modulos_Combobox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(31, 31, 31))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addComponent(Borrar_uno_boton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(23, 23, 23)))
+                        .addComponent(Agregar_Modulos)))
                 .addGap(18, 18, 18)
                 .addComponent(Borrar_Modulos)
                 .addGap(26, 26, 26))
@@ -142,7 +181,8 @@ public class Combobox extends javax.swing.JFrame {
             Modulos_Combobox.addItem(Campo_modulos.getText());
             reset();
         } else {
-            if (existe()) {
+            //Cuando el modulo ya existe manda un mensaje
+            if (existeModulo()) {
                 JOptionPane.showMessageDialog(null, "El modulo introducido ya existe");
                 reset();
                 return;
@@ -158,26 +198,50 @@ public class Combobox extends javax.swing.JFrame {
         //Si me creo los objetos aqui cada vez que borre todos los items se crearan dos veces
         //meter cada elemento del arraylist al combobox
         if (Modulos_Combobox.getItemCount() == 0) {
-            for (String it : modulos_matriculados) {
-                Modulos_Combobox.addItem(it);
+            if (Curso_Combobox.getSelectedItem() == "DAM1") {
+                for (String it : modulos_DAM1) {
+                    Modulos_Combobox.addItem(it);
+                }
+            }else {
+                for (String it : modulos_DAM2) {
+                    Modulos_Combobox.addItem(it);
+                }
             }
+
         } else {
-            JOptionPane.showMessageDialog(null, "TuPTM");
+            JOptionPane.showMessageDialog(null, "Ya has agregado todos los modulos");
         }
 
 
     }//GEN-LAST:event_Agregar_ModulosActionPerformed
 
     private void Borrar_ModulosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Borrar_ModulosActionPerformed
+        if(Modulos_Combobox.getItemCount() == 0){
+            JOptionPane.showMessageDialog(null, "No existe ningun modulo para borrar");
+            return;
+        }
         Modulos_Combobox.removeAllItems();
     }//GEN-LAST:event_Borrar_ModulosActionPerformed
+
+    private void Borrar_uno_botonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Borrar_uno_botonActionPerformed
+        if(Modulos_Combobox.getItemCount() == 0){
+            JOptionPane.showMessageDialog(null, "No existe ningun modulo para borrar");
+            return;
+        }
+        Modulos_Combobox.removeItem(Modulos_Combobox.getSelectedItem());
+    }//GEN-LAST:event_Borrar_uno_botonActionPerformed
+
+    private void Curso_ComboboxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_Curso_ComboboxActionPerformed
+
+
+    }//GEN-LAST:event_Curso_ComboboxActionPerformed
 
     /**
      * Comprueba que el modulo introducido no existe en el combobox
      *
      * @return
      */
-    private boolean existe() {
+    private boolean existeModulo() {
         for (int i = 0; i < Modulos_Combobox.getItemCount(); i++) {
             if (Modulos_Combobox.getItemAt(i).equalsIgnoreCase(Campo_modulos.getText())) {
                 return true;
@@ -185,7 +249,7 @@ public class Combobox extends javax.swing.JFrame {
         }
         return false;
     }
-
+  
     private void reset() {
         Campo_modulos.setText("");
     }
@@ -226,7 +290,9 @@ public class Combobox extends javax.swing.JFrame {
     private javax.swing.JButton Agregar_Modulos;
     private javax.swing.JButton Añadir_Boton;
     private javax.swing.JButton Borrar_Modulos;
+    private javax.swing.JButton Borrar_uno_boton;
     private javax.swing.JTextField Campo_modulos;
+    private javax.swing.JComboBox<String> Curso_Combobox;
     private javax.swing.JComboBox<String> Modulos_Combobox;
     private javax.swing.JLabel Texto_Modulos;
     // End of variables declaration//GEN-END:variables
